564a4b795e69f6c50f5ecb25fccc7fd09263f6cf
Marko <markovasicdeveloper@gmail.com>
2023-09-09 18:02:48 +0200

    Login background

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -1,21 +1,27 @@
 import { faMailBulk, faSignature } from '@fortawesome/free-solid-svg-icons';
 import Input from '../layout/input/Input';
 import { useInputText } from '../../hooks/useInputText';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from '../layout/button/Button';
 
 export default function LogIn(): JSX.Element {
     const { data, edit } = useInputText({});
 
-    useEffect(() => console.log(data), [data]);
+    const [disabled, setDisabled] = useState(true);
+
+    useEffect(() => {
+        if (!data.username || !data.password) return setDisabled(true);
+        setDisabled(false);
+    }, [data]);
 
     return (
         <section id="logIn">
             <div className="container">
-                <Input id={'username'} icon={faMailBulk} label='Korisnicko ime:' onChangeInput={edit} name={'username'} required />
-                <Input id={'password'} icon={faSignature} label='Lozinka' onChangeInput={edit} name={'password'} required />
+                <h2>Ulogujte se</h2>
+                <Input id={'username'} icon={faMailBulk} label='Korisnicko ime' onChangeInput={edit} name={'username'} placeholder='Korisnicko ime' required />
+                <Input id={'password'} icon={faSignature} label='Lozinka' onChangeInput={edit} name={'password'} placeholder='Lozinka' required />
+                <Button title={'Uloguj se'} onClickFunction={() => { }} type='submit' disabled={disabled} />
             </div>
-            <Button title={'Uloguj se'} onClickFunction={() => { }} type='submit' />
         </section>
     )
 }
